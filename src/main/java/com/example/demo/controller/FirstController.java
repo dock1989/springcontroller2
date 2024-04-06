4215a4d52cf00e24d188e08e218158a656f4fcbb
unknown <durgesh13june1989@gmail.com>
2024-04-06 11:35:52 +0530

    Added Form

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
 
 
+import com.example.demo.model.Faculty;
 import com.example.demo.model.Student;
-import com.example.demo.service.StudentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,33 +13,65 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
-   @Autowired
-    StudentService studentService;
 
-    @GetMapping("/check")
-    public ModelAndView check(Model m){
-        List<Student> studentList = studentService.getStudentData();
-        m.addAttribute("student",studentList);
-        return new ModelAndView("First");
 
-    }
+   @GetMapping("/health")
+   public String health(Model m){
+       return "health";
+   }
 
     @GetMapping("/showData")
     public ModelAndView showData(Model model){
         ModelAndView modelAndView = new ModelAndView("second");
-//        Map<String,String> map = new HashMap<>();
-//        map.put("one","Hello");
-//        map.put("two","data");
+        Map<String,String> map = new HashMap<>();
+        map.put("one","Hello");
+        map.put("two","data");
 
-        model.addAttribute("check","test");
+        model.addAttribute("check",map);
         return  modelAndView;
     }
+
+    @GetMapping("/showFaculty")
+    public String showFaculty(Model model){
+        Faculty faculty = new Faculty();
+        faculty.setName("Ajay");
+        faculty.setEmail("abc@gmail.com");
+        faculty.setAddress("GKP");
+        model.addAttribute("faculty",faculty);
+        return "faculty";
+    }
+
+    @GetMapping("/saveData")
+    public String saveData(Model model){
+       model.addAttribute("faculty",new Faculty());
+       return "form";
+    }
+
+    @PostMapping("/greeting")
+    public String showData(Model model,@ModelAttribute Faculty faculty){
+       model.addAttribute("faculty",faculty);
+       return "showRecord";
+    }
+
+
+   /* @GetMapping("/check")
+    public ModelAndView check(Model m){
+        List<Student> studentList = studentService.getStudentData();
+        m.addAttribute("student",studentList);
+        return new ModelAndView("first");
+
+    }
+
+
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model)
@@ -53,5 +86,5 @@ public class FirstController {
         System.out.println("-------------------------- " + student.getName());
         model.addAttribute("student",student);
         return "showStudentData";
-    }
+    }*/
 }
---
+++ src/main/java/com/example/demo/model/Faculty.java
@@ -0,0 +1,18 @@
+package com.example.demo.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Faculty {
+
+    private String name;
+    private String email;
+    private String address;
+
+
+}
